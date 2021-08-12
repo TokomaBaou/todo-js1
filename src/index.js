7df13de1a8fe06d7d3c3140c6cdefdbcf12d97dc
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-12 10:56:49 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+//追加ボタンを押した時に関数を追加していく
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  alert(inputText);
+};
+
+document.getElementById("add-button");
